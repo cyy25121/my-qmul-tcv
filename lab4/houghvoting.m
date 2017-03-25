6d228d36784c5cd1606445e7e7c83732307670c7
YY <y.chang@se16.qmul.ac.uk>
2017-03-25 21:38:28 +0000

    Added lab3/lab4.

---
+++ lab4/houghvoting.m
@@ -0,0 +1,19 @@
+function hough_array = houghvoting(patches,position,spa_scale,tem_scale,frame_num,flag_mat,struct_cb)
+% hough_array           : is a matrix with 7 rows, each column indicating the
+%                       : predicted(votes) spatial location, predicted (voted) start and end frames,
+%                       : the votes, bounding box values(scale compensated). Example: Let a descriptor matched with
+%                       : the codeword (with one offset) cast votes at the spatial location [x,y],
+%                       : temporal location [s,e] (predictions for start and end frames), bounding box values [b1 b2] (stored during training)
+%                       : with value(weight) 'v', then corresponding column in the
+%                       : matrix hough_array will be [x y s e v b1 b2]'.
+% patches               : i/p descriptors
+% position              : spatial location of the detected STIP                   
+% spa_scale             : spatial scale                      
+% tem_scale             : temporal scale
+% frame_num             : frame number at which STIP was detected
+% flag_mat              : refer to ism_test_voting.m
+
+%-----------------------------------------------------------------------------------------------------
+% Write your code here to compute the matrix hough_array
+%-----------------------------------------------------------------------------------------------------
+  
